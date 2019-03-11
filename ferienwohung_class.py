ae1f49c3752bb5c547e91aac71c6728d107646e3
Nertagon <nertagon@gmail.com>
2019-03-11 22:49:27 +0100

    Nichts ge'ndert

--- ferienwohung_class.py
+++ ferienwohung_class.py
@@ -36,11 +36,11 @@ class Wohnung:
         """
         Gibt den Preis der Ferienwohnung pro Übernachtung zurück
         """
-    def setPreis(self, preis):
-        """
-        Legt einen neuen Preis pro Übernachtung fest
-        """
-        self.__Preis = preis
+    # def setPreis(self, preis):
+    #     """
+    #     Legt einen neuen Preis pro Übernachtung fest
+    #     """
+    #     self.__Preis = preis
 
 wohnung1 = Wohnung("Meerblick", "Sylt",4,69)
 wohnung2 = Wohnung("Alpen-Panorama", "Füssen",5,74)
